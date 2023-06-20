01aad11c4c7ef669c8528e829cdd8ff171cd7334
HubertKorcala <korcalahubert@gmail.com>
2023-06-20 20:52:28 +0200

    Feat: init project with vite, ts, tailwind and daisyUI

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import HeaderNavBar from "./components/HeaderNavBar";
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <HeaderNavBar />
+    </div>
+  );
+}
+
+export default App;
