494d27cd92f59056ef9412372769e9af7f7c958d
HubertKorcala <korcalahubert@gmail.com>
2023-10-01 14:45:45 +0200

    feat: add Order Routes & Controller

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { protect, admin } from "../middleware/authMiddleware.js";
+const router = express.Router();
+import {
+  addOrderItems,
+  getMyOrders,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getAllOrders,
+} from "../controllers/orderController.js";
+
+router
+  .route("/")
+  .post(protect, addOrderItems)
+  .get(protect, admin, getAllOrders);
+
+router.route("/mine").get(protect, getMyOrders);
+
+router.route("/:id").get(protect, admin, getOrderById);
+
+router.route("/:id/pay").put(protect, updateOrderToPaid);
+
+router.route("/:id/deliver").put(protect, admin, updateOrderToDelivered);
+
+export default router;
